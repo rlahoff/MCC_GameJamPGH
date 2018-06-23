c684e4e8e7d8f5d1a380655153091bb361a06c9b
LAPTOP-URF7MCEU\TheQueen <kalaxander@yahoo.com>
2018-06-22 20:36:41 -0400

    With Penguin asset pack
    With Penguin asset pack

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string name)
+    {
+        //Debug.Log ("New Level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        //Debug.Log("Quit requested");
+        Application.Quit();
+    }
+}
