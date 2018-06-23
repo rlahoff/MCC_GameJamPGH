f713f94d220d1af79ab812dde8ec4d108cf607b9
LAPTOP-URF7MCEU\TheQueen <kalaxander@yahoo.com>
2018-06-23 19:57:07 -0400

    Camera Follows Player
    Camera Follows Player

---
+++ Assets/1_Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        player = GameObject.Find("Player");
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        // Debug.Log(transform.position.x);
+        float xpos = player.transform.position.x + offset.x;
+        transform.position = new Vector3 (Mathf.Clamp(xpos, 0f, 1000f), transform.position.y, transform.position.z);
+    }
+}
