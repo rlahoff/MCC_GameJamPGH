d56a6441c7aabf398df74e6438740d08439048f5
LAPTOP-URF7MCEU\TheQueen <kalaxander@yahoo.com>
2018-06-24 07:00:45 -0400

    Camera follow fixes
    Penguin always on top
    Level 02 camera size to 10
    Camera boundary fixes with LevelBoundary

--- Assets/1_Scripts/CameraFollow.cs
+++ Assets/1_Scripts/CameraFollow.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
     public GameObject player;       //Public variable to store a reference to the player game object
-
-
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // keep camera bounds inside level
+    float xMin;
+    float xMax;
+    float xMinOffsetFromEdge = 20;  // TODO probably better to calculate this
+    float xOffsetFromPlayer = 10;  // TODO probably better to calculate this
+
+    //float yMin;
+    //float yMax;
+    //float yMinOffset;
 
     // Use this for initialization
     void Start()
@@ -15,6 +23,27 @@ public class CameraFollow : MonoBehaviour {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         player = GameObject.Find("Player");
         offset = transform.position - player.transform.position;
+
+        GameObject levelBoundaryLowerLeft = GameObject.Find("LevelBoundaryLowerLeft");
+        GameObject levelBoundaryUpperRight = GameObject.Find("LevelBoundaryUpperRight");
+
+        Debug.Log("Screen Width = " + Screen.width + "  Screen Width = " + Screen.height);
+
+        if (!levelBoundaryLowerLeft)
+            Debug.LogWarning("Place a LevelBoundary named LevelBoundaryLowerLeft on this level");
+        if (!levelBoundaryUpperRight)
+            Debug.LogWarning("Place a LevelBoundary named LevelBoundaryUpperRight on this level");
+
+        //Debug.Log(levelBoundaryLowerLeft.transform.position );
+        //Debug.Log(levelBoundaryUpperRight.transform.position);
+
+        xMin = levelBoundaryLowerLeft.transform.position.x + xMinOffsetFromEdge;
+        xMax = levelBoundaryUpperRight.transform.position.x - xMinOffsetFromEdge;
+
+        //Debug.Log(xMin + " " + xMax);
+        //yMin = levelBoundaryLowerLeft.transform.position.y + yMinOffset;
+        //yMax = levelBoundaryUpperRight.transform.position.y - yMinOffset;
+
     }
 
     // LateUpdate is called after Update each frame
@@ -22,7 +51,10 @@ public class CameraFollow : MonoBehaviour {
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         // Debug.Log(transform.position.x);
-        float xpos = player.transform.position.x + offset.x - 10;
-        transform.position = new Vector3 (Mathf.Clamp(xpos, 0f, 1000f), transform.position.y, transform.position.z);
+
+        // currently only following the player in x
+        float xpos = player.transform.position.x + offset.x - xOffsetFromPlayer;
+        transform.position = new Vector3(Mathf.Clamp(xpos, xMin, xMax), transform.position.y, transform.position.z);
+
     }
 }
