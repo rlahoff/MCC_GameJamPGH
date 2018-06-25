d27717cbfb7f10202f42c6f86771a6a63b32b304
LAPTOP-URF7MCEU\TheQueen <kalaxander@yahoo.com>
2018-06-25 16:11:04 -0400

    Green snowflake and colorray
    Green snowflake and colorray
    still in progress

--- Assets/1_Scripts/Player.cs
+++ Assets/1_Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    //[SerializeField] Snowflake.COLOR my_Color = Snowflake.COLOR.Yellow;
+    [SerializeField] COLOR my_Color = COLOR.Yellow;
+
     public GameObject colorRay;
 
     public float speed = 8;
@@ -87,20 +90,35 @@ public class Player : MonoBehaviour {
         //Goal goal = collision.gameObject.GetComponent<Goal>();
         //Debug.Log("OnTriggerEnter2D");
 
-        if (collision.name == "Goal")
-        //if (goal)
-        {
-            GameObject levelManagerGO = GameObject.Find("LevelManager");
+        Debug.Log(collision.name);
 
-            if (levelManagerGO)
-            {
-                LevelManager levelManager = levelManagerGO.GetComponent<LevelManager>();
-                levelManager.LoadNextLevel();
-            }
-            else
-                Debug.LogWarning("Place a LevelManager prefab on this level");
-            
-                
+        if (collision.name == "Goal")
+        {
+            TriggerGoal();
         }
+        else if (collision.name.Contains("Snowflake"))
+        {
+            Snowflake snowflake = collision.gameObject.GetComponent<Snowflake>();
+            TriggerSnowflake(snowflake.Color());
+        }
+    }
+
+    private void TriggerSnowflake(COLOR color)
+    {
+        if (my_Color == color) return;
+
+    }
+
+    private void TriggerGoal()
+    {
+        GameObject levelManagerGO = GameObject.Find("LevelManager");
+
+        if (levelManagerGO)
+        {
+            LevelManager levelManager = levelManagerGO.GetComponent<LevelManager>();
+            levelManager.LoadNextLevel();
+        }
+        else
+            Debug.LogWarning("Place a LevelManager prefab on this level");
     }
 }
