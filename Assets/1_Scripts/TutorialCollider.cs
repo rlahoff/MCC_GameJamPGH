c574d5643242bedcc289043c9eed9486a6245e53
LAPTOP-URF7MCEU\TheQueen <kalaxander@yahoo.com>
2018-06-26 18:54:42 -0400

    Tutorial text added
    Tutorial text added, and tutorial level added to build

---
+++ Assets/1_Scripts/TutorialCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+
+        if ( player )
+        {
+            GameObject tutorialText = GameObject.Find("Tutorial Text");
+            tutorialText.GetComponent< TutorialText>().NextText();
+
+        }
+    }
+}
