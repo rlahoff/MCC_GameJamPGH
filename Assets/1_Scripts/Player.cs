8ad0466231db8b751b77709178cce48fea5698cd
LAPTOP-URF7MCEU\TheQueen <kalaxander@yahoo.com>
2018-06-27 16:16:44 -0400

    penguin diagonal move & fire
    penguin diagonal move & fire

--- Assets/1_Scripts/Player.cs
+++ Assets/1_Scripts/Player.cs
@@ -18,8 +18,11 @@ public class Player : MonoBehaviour {
     public float speed = 8;
     public float rayFiringRate = 0.2f;
 
-    enum Facing { LEFT, RIGHT };
-    private Facing facing = Facing.RIGHT;
+    enum Facing { NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST };
+    
+    private Facing my_facing = Facing.EAST;
+    //private Facing my_facing = Facing.EAST;
+    private Facing my_oldFacing = Facing.EAST;
 
     static float playingTimeElapsed = 0;
     static int totalSharks = 0;
@@ -93,37 +96,38 @@ public class Player : MonoBehaviour {
 
     private void ProcessInput()
     {
+        bool moveRight = false;
+        bool moveLeft = false;
+        bool moveUp = false;
+        bool moveDown = false;
+
+        bool moving = false;
+
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            if (facing != Facing.LEFT)
-            {
-                facing = Facing.LEFT;
-                //bool flip = GetComponent<SpriteRenderer>().flipX;
-                GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
-            }
-            
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            moveLeft = true;
+            moving = true;
         }
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            if (facing != Facing.RIGHT)
-            {
-                facing = Facing.RIGHT;
-                //bool flip = GetComponent<SpriteRenderer>().flipX;
-                GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
-            }
-
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            moveRight = true;
+            moving = true;
         }
 
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
+            moveUp = true;
+            moving = true;
         }
         else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            moveDown = true;
+            moving = true;
         }
+
+        //todo, support number pad?
+        
+        if (moving) MovePlayer(moveRight, moveLeft, moveUp, moveDown);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -135,8 +139,122 @@ public class Player : MonoBehaviour {
         }
     }
 
+    private void MovePlayer(bool moveRight, bool moveLeft, bool moveUp, bool moveDown)
+    {
+        my_oldFacing = my_facing;
+
+        if (moveLeft)
+        {
+            if (moveUp)
+                my_facing = Facing.NORTHWEST;
+            else if (moveDown)
+                my_facing = Facing.SOUTHWEST;
+            else
+                my_facing = Facing.WEST;
+        }
+        else if (moveRight)
+        {
+            if (moveUp)
+                my_facing = Facing.NORTHEAST;
+            else if (moveDown)
+                my_facing = Facing.SOUTHEAST;
+            else
+                my_facing = Facing.EAST;
+        }
+        else if (moveUp)
+            my_facing = Facing.NORTH;
+        else
+            my_facing = Facing.SOUTH;
+
+       // Debug.Log("facing: " + my_facing);
+
+        Vector3 moveVector = FacingVector(my_facing);
+       
+        // move
+        transform.position += moveVector * speed * Time.deltaTime;
+
+        // rotate
+        if (my_facing != my_oldFacing)
+        {
+            Vector3 rotateVector = RotationVector(my_facing);
+            transform.Rotate(rotateVector);
+
+            //enum Facing { NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST };
+            int[] flip = new int[] { 0, 0, 0, 0, 0, 1, 1, 1 };
+            if (flip[(int)my_facing] != flip[(int)my_oldFacing])
+                //bool flip = GetComponent<SpriteRenderer>().flipX;
+                GetComponent<SpriteRenderer>().flipY = !GetComponent<SpriteRenderer>().flipY;
+
+
+        }
+    }
+
+    private Vector3 RotationVector(Facing facing)
+    {
+        //enum Facing { NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST };
+
+        int steps;
+
+        // do not allow facing up or down (when moving up or down have to face east or west)
+        if (my_facing == Facing.NORTH || my_facing == Facing.SOUTH)
+        {
+            if (my_oldFacing > Facing.NORTH && my_oldFacing < Facing.SOUTH)
+                my_facing = Facing.EAST;
+            else
+                my_facing = Facing.WEST;
+        }
+
+        steps = my_oldFacing - my_facing;
+        //Debug.Log(steps);
+
+        return new Vector3(0, 0, steps * 45);
+    }
+
+    private Vector3 FacingVector(Facing facing)
+    {
+        Vector3 moveVector;
+
+        switch (facing)
+        {
+            case (Facing.NORTH):
+                moveVector = new Vector3(0, 1, 0);
+                break;
+            case (Facing.NORTHEAST):
+                moveVector = new Vector3(1, 1, 0);
+                break;
+            case (Facing.EAST):
+                moveVector = new Vector3(1, 0, 0);
+                break;
+            case (Facing.SOUTHEAST):
+                moveVector = new Vector3(1, -1, 0);
+                break;
+            case (Facing.SOUTH):
+                moveVector = new Vector3(0, -1, 0);
+                break;
+            case (Facing.SOUTHWEST):
+                moveVector = new Vector3(-1, -1, 0);
+                break;
+            case (Facing.WEST):
+                moveVector = new Vector3(-1, 0, 0);
+                break;
+            case (Facing.NORTHWEST):
+                moveVector = new Vector3(-1, 1, 0);
+                break;
+            default:    // should never get here
+                Debug.LogWarning("unhandled facing");
+                moveVector = new Vector3(0, 0, 0);
+                break;
+        }
+        //Debug.Log(moveVector);
+        return moveVector;
+    }
+
     private void FireColorRay()
     {
+        // on my computer if i'm holding UP and LEFT at the same time then SPACEBAR doesn't work
+        // but I'm fine using WA or the numberkey pad with numlock off
+
+        //Debug.Log("FireColorRay " + my_facing);
         GameObject beam;
 
         beam = Instantiate(colorRayPrefabs[(int)my_Color], transform.position, Quaternion.identity) as GameObject;
@@ -144,10 +262,15 @@ public class Player : MonoBehaviour {
         float speed = beam.gameObject.GetComponent<ColorRay>().GetSpeed();
         beam.gameObject.GetComponent<ColorRay>().SetColor(my_Color);
 
-        if (facing == Facing.RIGHT)
-            beam.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
-        else
-            beam.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+
+        //enum Facing { NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST };
+        Vector3 facingVector = FacingVector(my_facing);
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector2(speed * facingVector.x, speed * facingVector.y);
+
+        //       if (facing == Facing.RIGHT)
+        //beam.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+ //       else
+ //           beam.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
 
         //AudioSource.PlayClipAtPoint(fireSound, transform.position);
 
